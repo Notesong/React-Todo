c3367aeceaa591e89c35bd144b1f54fbf1ce99a6
Lee Damien <notesong.git@outlook.com>
2020-01-16 01:09:01 -0500

    Add comments

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Todo = props => {
     return (
         <div 
+            // let user declare task completed by clicking it
             onClick={e => props.toggleCompleted(props.item.id)}
+            // changes css based on whether task completed
             className={`task${props.item.completed ? " completed" : ""}`}
         >
             <p>{props.item.task}</p>
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 class TodoForm extends React.Component {
+
+    // sets up an empty new task state
     state = {
         newTask: ''
     };
 
+    // handles any input changes in the new task form
     handleChanges = e => {
         this.setState({...this.state, newTask: e.target.value});
     };
 
+    // adds the new task to the task list and resets the input field
     handleSubmit = e => {
         e.preventDefault();
         this.props.addNewTask(this.state.newTask);
@@ -18,6 +22,7 @@ class TodoForm extends React.Component {
     render() {
         return(
             <div className='form-todo'>
+                {/* new task form */}
                 <form onSubmit={this.handleSubmit}>
                     <input
                         onChange={this.handleChanges}
@@ -26,15 +31,16 @@ class TodoForm extends React.Component {
                         value={this.state.newTask}
                         maxLength='200'
                     />
-                    <button className='btn-add'>Add</button>                    
+                    <button className='btn'>Add</button>                    
                 </form>
-                <button className='btn-clear' onClick={this.props.saveData}>
+                {/* save, load, and detete selected buttons */}
+                <button className='btn' onClick={this.props.saveData}>
                     Save
                 </button>
-                <button className='btn-clear' onClick={this.props.loadData}>
+                <button className='btn' onClick={this.props.loadData}>
                     Load
                 </button>
-                <button className='btn-clear' onClick={this.props.clearCompleted}>
+                <button className='btn' onClick={this.props.clearCompleted}>
                     Delete Selected
                 </button>
             </div>
